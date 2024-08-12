865224aef95d649a43b4c4d0d0797fdb7e349b67
GitHub Action <action@github.com>
2024-08-12 20:36:30 +0000

    Bump version to 1.1.8-dev

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from pymongo import MongoClient
 # Load environment variables from .env file
 load_dotenv()
 
-VERSION = "1.1.7-dev"
+VERSION = "1.1.8-dev"
 REDIS_ENDPOINT = environ.get("REDIS_ENDPOINT", "localhost")
 REDIS_PORT = int(environ.get("REDIS_PORT", "6379"))
 MONGO_URI = environ.get("MONGO_URI")
